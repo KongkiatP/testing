b90f0165b33ec793699e9dccac64531c75c4ee1c
ph.kongkiat <ph.kongkiat@gmail.com>
2023-02-06 23:26:50 +0700

    [TECH] - create unit tests for function minus at service

--- src/test/java/com/king/mongkut/testing/controller/CalculationControllerTest.java
+++ src/test/java/com/king/mongkut/testing/controller/CalculationControllerTest.java
@@ -16,4 +16,6 @@ class CalculationControllerTest {
 
     @InjectMocks
     private CalculationController calculationController;
+
+
 }
--- src/test/java/com/king/mongkut/testing/service/CalculationServiceTest.java
+++ src/test/java/com/king/mongkut/testing/service/CalculationServiceTest.java
@@ -1,5 +1,6 @@
 package com.king.mongkut.testing.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,4 +12,24 @@ class CalculationServiceTest {
 
     @InjectMocks
     private CalculationService calculationService;
+
+    @Test
+    void should_return_number_when_call_minus_given_number1_and_number2() {
+        // Given
+        // When
+        int actual = calculationService.minus("10", "5");
+
+        // Then
+        assertEquals(5, actual);
+    }
+
+    @Test
+    void should_return_minus_one_when_call_minus_given_number1_is_X() {
+        // Given
+        // When
+        int actual = calculationService.minus("X", "5");
+
+        // Then
+        assertEquals(-1, actual);
+    }
 }
