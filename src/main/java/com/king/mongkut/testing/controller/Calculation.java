6d2f2a628ebca7d4773c2670e16d520e44f0f810
ph.kongkiat <ph.kongkiat@gmail.com>
2023-02-06 22:47:44 +0700

    [TECH] - update rest controller class and create input model

--- src/main/java/com/king/mongkut/testing/controller/Calculation.java
+++ src/main/java/com/king/mongkut/testing/controller/Calculation.java
@@ -1,5 +1,18 @@
 package com.king.mongkut.testing.controller;
 
+import com.king.mongkut.testing.model.NumberInput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequiredArgsConstructor
 public class Calculation {
+
+    @RequestMapping(value = "plus", method = RequestMethod.GET)
+    public ResponseEntity<?> plus(@RequestBody NumberInput numberInput) {
+        {
+            return ResponseEntity.ok("login");
+        }
+    }
 }
---
+++ src/main/java/com/king/mongkut/testing/model/NumberInput.java
@@ -0,0 +1,13 @@
+package com.king.mongkut.testing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NumberInput {
+}
