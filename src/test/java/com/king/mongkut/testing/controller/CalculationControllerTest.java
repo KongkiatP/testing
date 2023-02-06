20deaa7b67988101a03448e16318793499163efc
ph.kongkiat <ph.kongkiat@gmail.com>
2023-02-06 23:22:25 +0700

    [TECH] - create test cases for controller and service

---
+++ src/test/java/com/king/mongkut/testing/controller/CalculationControllerTest.java
@@ -0,0 +1,19 @@
+package com.king.mongkut.testing.controller;
+
+import com.king.mongkut.testing.service.CalculationService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CalculationControllerTest {
+
+    @Mock
+    private CalculationService calculationService;
+
+    @InjectMocks
+    private CalculationController calculationController;
+}
