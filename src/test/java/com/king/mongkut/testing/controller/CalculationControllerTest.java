b98d6e2c3b9c5706632bc93c6c2858b39c047b63
DESKTOP-VETK7PM\mangm <garontiger@gmail.com>
2023-02-06 23:36:00 +0700

    update unit test service and controller

--- src/test/java/com/king/mongkut/testing/controller/CalculationControllerTest.java
+++ src/test/java/com/king/mongkut/testing/controller/CalculationControllerTest.java
@@ -1,12 +1,15 @@
 package com.king.mongkut.testing.controller;
 
 import com.king.mongkut.testing.service.CalculationService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CalculationControllerTest {
@@ -16,4 +19,16 @@ class CalculationControllerTest {
 
     @InjectMocks
     private CalculationController calculationController;
+
+
+    @Test
+    void should_return_success_when_call_plus_with_number1_and_number2(){
+        when(calculationService.plus("1","2")).thenReturn(3);
+//        ResponseEntity entity = calculationController.plus("2","3");
+
+    }
+    @Test
+    void should_return_error_when_call_plus_with_number1_and_number2(){
+//        assertThrows(NumberFormatException.class,()-> {calculationService.plus("b","3");});
+    }
 }
--- src/test/java/com/king/mongkut/testing/service/CalculationServiceTest.java
+++ src/test/java/com/king/mongkut/testing/service/CalculationServiceTest.java
@@ -1,5 +1,6 @@
 package com.king.mongkut.testing.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,4 +12,15 @@ class CalculationServiceTest {
 
     @InjectMocks
     private CalculationService calculationService;
+
+    @Test
+    void should_return_success_when_call_plus_with_number1_and_number2(){
+        int result = calculationService.plus("2","3");
+        assertEquals(5,result);
+    }
+
+    @Test
+    void should_return_error_when_call_plus_with_number1_and_number2(){
+        assertThrows(NumberFormatException.class,()-> {calculationService.plus("b","3");});
+    }
 }
