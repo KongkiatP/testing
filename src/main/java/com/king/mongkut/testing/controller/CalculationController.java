ee33377059e36332cb7912d8dd335a078ae1c5f3
ph.kongkiat <ph.kongkiat@gmail.com>
2023-02-06 22:52:20 +0700

    [TECH] - create new service for calculation

--- src/main/java/com/king/mongkut/testing/controller/CalculationController.java
+++ src/main/java/com/king/mongkut/testing/controller/CalculationController.java
@@ -1,13 +1,23 @@
 package com.king.mongkut.testing.controller;
 
 import com.king.mongkut.testing.model.NumberInput;
-import lombok.RequiredArgsConstructor;
+import com.king.mongkut.testing.service.CalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class CalculationController {
+
+    private final CalculationService calculationService;
+
+    @Autowired
+    public CalculationController(CalculationService calculationService) {
+        this.calculationService = calculationService;
+    }
 
     @RequestMapping(value = "plus", method = RequestMethod.GET)
     public ResponseEntity<?> plus(@RequestBody NumberInput numberInput) {
---
+++ src/main/java/com/king/mongkut/testing/service/CalculationService.java
@@ -0,0 +1,11 @@
+package com.king.mongkut.testing.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculationService {
+
+    public int plus() {
+        return 0;
+    }
+}
