ff51faebab17e82e1c238b5cb552a5ac98471034
ph.kongkiat <ph.kongkiat@gmail.com>
2023-02-06 23:01:45 +0700

    [TECH] - remove unused annotations

--- src/main/java/com/king/mongkut/testing/model/NumberInput.java
+++ src/main/java/com/king/mongkut/testing/model/NumberInput.java
@@ -1,13 +1,11 @@
 package com.king.mongkut.testing.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class NumberInput {
+
+    private int number1;
 }
